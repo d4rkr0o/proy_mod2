3ff0013e5cee99f7c8d62ad016a0a4f51551c94b
craguilar <craguilardf@hotmail.com>
2014-03-18 19:32:58 -0600

    Script de creacion de BD

---
+++ web-app/bd/script_bd.sql
@@ -0,0 +1,14 @@
+  CREATE TABLE Usuarios (
+    idUsuarios INTEGER   NOT NULL ,
+    Usuario VARCHAR(45)   NOT NULL ,
+    Contrasena VARCHAR(45)   NOT NULL ,
+    Camp_1 VARCHAR(45)    ,
+    Camp_2 VARCHAR(45)    ,
+    Camp_3 VARCHAR(45)    ,
+    Camp_4 VARCHAR(45)    ,
+    Camp_5 VARCHAR(45)    ,
+    Camp_6 VARCHAR(45)    , 
+    Camp_7 VARCHAR(45)    , 
+    Camp_8 VARCHAR(45)    ,
+    Camp_9 VARCHAR(45)      ,
+  PRIMARY KEY(idUsuarios));
